e217ddf5a634b1df7d802dec5c19af2b93c308e9
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-08-12 15:35:49 -0500

    got the create project button to work

--- main.js
+++ main.js
@@ -13,6 +13,41 @@ const navContainerElement = document.querySelector("#navContainer")
 const profileArea = document.querySelector("#profile-area")
 //const overviewBtnelement = document.querySelector("overviewBtn")
 
+//these select the DOM elements with the ID "proj" and "projectSearchInput" and stores them in a variable - LM
+const projectsArea = document.querySelector("#proj");
+const projectSearchInput = document.querySelector("#projectSearchInput");
+const projectForm = document.querySelector("#projectForm");
+
+//this defines a function to render the list of projects on the webpage
+function renderProjects(projects) {
+  let domString = '<ul class="list-group">';
+
+  //this goes through each project and builds the HTML representation
+  projects.forEach(project => {
+    domString += `
+    <li class="list-group-item">
+    <div class="title-column">${project.title}</div>
+    <div class="description-column">${project.description}</div>
+    </li>`;
+  });
+
+  domString += "</ul>";
+
+  //this updates the HTML of the projects area with the generated HTML
+  projectsArea.innerHTML = domString;
+}
+
+//this defines a function to filter and display projects based on a search term
+function searchProjects(searchTerm) {
+
+  //this filters projects whose titles contain the search term and is case sensitive
+  const filteredProjects = projectData.filter(project => {
+    return project.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  //this renders the filtered projects
+  renderProjects(filteredProjects);
+} //-LM
 
 //Functions
 
@@ -147,10 +182,24 @@ function eventListeners(){
       console.log("Create Project Button Clicked!")
     }
     
-  })
+  });
+
+    //this renders all projects initially
+    renderProjects(projectData);
+
+    //this adds an event listener to the search input field
+    projectSearchInput.addEventListener("input", event => {
+  
+      //this gets the current input value
+      const searchTerm = event.target.value;
+  
+      //this calls the searchProjects function to filter and display projects
+      searchProjects(searchTerm);
+    });
 }
 
 // Rendering (later to be modularized)
 renderFooter();
 renderProfile();
 eventListeners();
+renderProjects(projectData);
